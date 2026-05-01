07e4bffeeb60956c282d6e776b5f77980d6ee7a6
SethMartin-789 <2533376@etu.cchic.ca>
2026-05-01 08:52:41 -0400

    Add ErrorSystem class and its ShowCriticalError method.

---
+++ ConsoleLib/ErrorSystem.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleLib;
+
+internal class ErrorSystem
+{
+    public static void ShowCriticalError(string smallErrorDescription, bool inFrench = false)
+    {
+        if (inFrench)
+        {
+            Console.WriteLine(
+                $"\nAVERTISSEMENT : une erreur {smallErrorDescription} mène à l'incapacité " +
+                 "de poursuivre les opérations de ce programme.\nFermeture du programme...");
+        }
+        else
+        {
+            Console.WriteLine(
+                $"\nWARNING : a critical error {smallErrorDescription} prevents the program " +
+                 "from pursuing operations.\nProgram exiting...");
+        }
+    }
+}
