4768547858c5779490fe14c2b14d2841b218be0c
SethMartin-789 <2533376@etu.cchic.ca>
2026-05-01 09:24:24 -0400

    Make the class ErrorSystem static and public while also adjusting the french error messaging.

--- ConsoleLib/ErrorSystem.cs
+++ ConsoleLib/ErrorSystem.cs
@@ -1,13 +1,13 @@
 ﻿namespace ConsoleLib;
 
-internal class ErrorSystem
+public static class ErrorSystem
 {
     public static void ShowCriticalError(string smallErrorDescription, bool inFrench = false)
     {
         if (inFrench)
         {
             Console.WriteLine(
-                $"\nAVERTISSEMENT : une erreur {smallErrorDescription} mène à l'incapacité " +
+                $"\nAVERTISSEMENT : une erreur critique {smallErrorDescription} mène à l'incapacité " +
                  "de poursuivre les opérations de ce programme.\nFermeture du programme...");
         }
         else
